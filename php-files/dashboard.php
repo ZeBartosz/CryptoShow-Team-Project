a06c32c664eb601ed01f73f8800ecbbb208339d7
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-31 20:40:42 +0100

    Fix session check and Add elements from index.html

--- php-files/dashboard.php
+++ php-files/dashboard.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
+if (isset($_SESSION['user_id'])) {
 } else {
-    header("Location: ../login.php");
+    header("Location: ../login.html");
     session_destroy();
     exit();
 }
@@ -15,6 +15,7 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="../css-files/dashboardStyle.css">
+    <script src="../Js-Files/carousel.js" defer></script>
 </head>
 <body>
     <header>
@@ -23,9 +24,38 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
             <li><a href="./profile.php">Profile</a></li>
         </ul>
         <form action="logout.php" method="post">
-            <button type="submit">Logout</button>
+            <button class="logoutBtn" type="submit">Logout</button>
         </form>
     </header>
-    
+    <div class="container">
+        <div class="carousel-container">
+            <div class="carousel">
+                <div class="event-image"><img src="https://fakeimg.pl/960x400?text=1" alt="Slide 1"></div>
+                <div class="event-image"><img src="https://fakeimg.pl/960x400?text=2" alt="Slide 2"></div>
+                <div class="event-image"><img src="https://fakeimg.pl/960x400?text=3" alt="Slide 3"></div>
+            </div>
+            <div class="carousel-arrows">
+                <button class="arrow-btn prev" onclick="prevImage()"></button>
+                <button class="arrow-btn next" onclick="nextImage()"></button>
+            </div>
+        </div>
+    </div>
+    <div class="website-features">
+        <button class="features">
+            <h2>Lorem ipsum</h2>
+            <p>Lorem ipsum</p>
+        <button class="features">
+            <h2>Lorem ipsum</h2>
+            <p>Lorem ipsum</p>
+        <button class="features">
+            <h2>Lorem ipsum</h2>
+            <p>Lorem ipsum</p>
+    </div>
+    <footer>
+        <a href="#">About Us |</a>
+        <a href="#">Contact Us |</a>
+        <a href="#">Learn More</a>
+        <p>&copy; 2024 CryptoShow. All rights reserved.</p>
+    </footer>
 </body>
 </html>
