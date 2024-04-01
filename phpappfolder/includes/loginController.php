790bad2fc057a509f09c26929d0dcb77d265e097
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-01 23:15:34 +0100

    Add login controller that extends login class

---
+++ phpappfolder/includes/loginController.php
@@ -0,0 +1,30 @@
+<?php
+
+include "loginClasses.php";
+class LoginControl extends Login {
+    private $userNickname;
+    private $pwd;
+
+    public function __construct($userNickname, $pwd) {
+        $this->userNickname = $userNickname;
+        $this->pwd = $pwd;
+    }
+
+    public function loginUser() {
+        if(!$this->emptyInput() == false) {
+            header("location: ../loginPage.php?error=emptyInput");
+            exit();
+        }
+        $this->getUser($this->userNickname, $this->pwd);
+    }
+
+    private function emptyInput(){
+        $result;
+        if(empty($this->userNickname) || empty($this->pwd)) {
+            $result = true;
+        } else {
+            $result = false;
+        }
+        return $result;
+    }
+}
