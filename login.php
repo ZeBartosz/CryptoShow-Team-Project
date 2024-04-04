58126a8531ee208b6d94a9db189ae3d199aa6af0
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-04 16:46:44 +0100

    Rename login page to .php and add header to the top

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+$title = "Login page";
+$css_file = "./css-files/dashboardStyle.css";
+include_once "header.php";
+?>
+    <div class="body">
+        <form action="./php-files/loginProcess.php" method="post">
+            <label for="username">Username or Email:</label><br>
+            <input type="text" id="username" name="username"><br>
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password"><br><br>
+            <input type="submit" name="submit">
+        </form>
+        <a href="./register.html">Register here</a>
+    </div>
+</body>
+</html>
