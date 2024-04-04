a5f995ab1b9f597649e8616400f64c236dc2816a
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-04 16:47:48 +0100

    Move and add header to the top

---
+++ profile.php
@@ -0,0 +1,21 @@
+<?php
+$title = "Profile page";
+$css_file = "./css-files/dashboardStyle.css";
+include_once "header.php";
+include "profileinfo.php";
+include "profileinfo_contrl.php";
+include "profileinfo_view.php";
+$profileInfo = new ProfileInfoView()
+?>
+<section>
+    <h1>User nickname: <?php $profileInfo->fetchNickname($_SESSION["user_id"])?></h1>
+    <p>User name: <?php $profileInfo->fetchName($_SESSION["user_id"])?></p>
+    <p>User email: <?php $profileInfo->fetchEmail($_SESSION["user_id"])?></p>
+    <a href="profileSetting.php">Edit Profile</a>
+</section>
+<section>
+    <p>User device count: <?php $profileInfo->fetchDeivceCount($_SESSION["user_id"])?>/5</p>
+</section>
+
+</body>
+</html>
---
+++ profileSetting.php
@@ -0,0 +1,31 @@
+<?php
+    $title = "Edit your profile";
+    $css_file = "./css-files/dashboardStyle.css";
+    include_once "header.php";
+    include "profileinfo.php";
+    include "profileinfo_contrl.php";
+    include "profileinfo_view.php";
+    $profileInfo = new ProfileInfoView();
+?>
+    <section class="profile">
+        <div class="profile-bg">
+            <div class="wrapper">
+                <div class="profile-settings">
+                    <h3>PROFILE SETTINGS</h3>
+                    <form action="profileSettingProcess.php" method="post">
+                        <P>Change your nickname!</P>
+                        <input type="text" name="nickname" placeholder="User nickname..." value="<?php $profileInfo->fetchNickname($_SESSION["user_id"])?>">
+                        <p>Change your name!</p>
+                        <input type="text" name="name" placeholder="User name..." value="<?php $profileInfo->fetchName($_SESSION["user_id"])?>">
+                        <p>Change your email!</p>
+                        <input type="text" name="email" placeholder="User email..." value="<?php $profileInfo->fetchEmail($_SESSION["user_id"])?>">
+                        <button type="submit" name="submit">SAVE</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</body>
+</html>
