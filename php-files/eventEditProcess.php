e86fc6e43ee2510cb6998e575b55550150b6fd97
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-06 18:04:24 +0100

    Add process to edit an event

---
+++ php-files/eventEditProcess.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+require_once "validateAdmin.php";
+include "db_connect.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if(isset($_POST["publish"])) {
+    $event_id = $_POST["publish"];
+
+    try {
+        $query = "UPDATE event SET is_published=true WHERE event_id=:id";
+        $stmt = $pdo->prepare($query);
+
+        $data = [
+          ":id" => $event_id,
+        ];
+
+        $result = $stmt->execute($data);
+
+        if($result) {
+            $_SESSION["message"] = "Published Successfully";
+            header("location: ../admin.php");
+            exit();
+        } else {
+            $_SESSION["message"] = "Not Published";
+            header("location: ../admin.php");
+            exit();
+        }
+
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+if(isset($_POST["delete"])) {
+    $event_id = $_POST["delete"];
+
+    try {
+
+        $query = "DELETE FROM event WHERE event_id=:id";
+        $stmt = $pdo->prepare($query);
+
+        $data = [
+            ":id" => $event_id,
+        ];
+        $result = $stmt->execute($data);
+
+        if($result) {
+            $_SESSION["message"] = "Deleted Successfully";
+            header("location: ../admin.php");
+            exit();
+        } else {
+            $_SESSION["message"] = "Not Deleted";
+            header("location: ../admin.php");
+            exit();
+        }
+
+    } catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+}
+
+if(isset($_POST["submit"])) {
+
+    $id = $_POST["id"];
+    $name = $_POST["name"];
+    $date = $_POST["date"];
+    $venue = $_POST["venue"];
+
+    try {
+        $query = "UPDATE event SET event_name=:name, event_date=:date, event_venue=:venue WHERE event_id=:id";
+
+        $stmt = $pdo->prepare($query);
+
+        $data = [
+            ":name" => $name,
+            ":date" => $date,
+            ":venue" => $venue,
+            ":id" => $id,
+        ];
+
+        $result = $stmt->execute($data);
+
+        if($result) {
+            $_SESSION["message"] = "Saved Successfully";
+            header("location: ../admin.php");
+            exit();
+        } else {
+            $_SESSION["message"] = "Not Saved";
+            header("location: ../admin.php");
+            exit();
+        }
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+
+
+}
