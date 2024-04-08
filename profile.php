c43a70c57528803850207b0c71d33078560cab6a
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2024-04-08 23:43:27 +0100

    Device are now shown on profile

--- profile.php
+++ profile.php
@@ -1,23 +1,51 @@
 <?php
 $title = "Profile page";
 $css_file = "./css-files/dashboardStyle.css";
+$css_file2 = "./css-files/profileStyle.css";
 include_once "header.php";
 include "profileinfo.php";
 include "profileinfo_contrl.php";
 include "profileinfo_view.php";
-$profileInfo = new ProfileInfoView()
-?>
-<section>
-    <h1>User nickname: <?php $profileInfo->fetchNickname($_SESSION["user_id"])?></h1>
-    <p>User name: <?php $profileInfo->fetchName($_SESSION["user_id"])?></p>
-    <p>User email: <?php $profileInfo->fetchEmail($_SESSION["user_id"])?></p>
-    <a href="profileSetting.php">Edit Profile</a>
-</section>
+include "deviceProcess.php";
+include "deviceController.php";
+include "deviceView.php";
+$profileInfo = new ProfileInfoView();
+$deviceInfo = new DeviceView();
 
-<section>
-    <p>User device count: <?php $profileInfo->fetchDeivceCount($_SESSION["user_id"])?>/5</p>
-    <a href="deviceAdd.php">Add Device</a>
-</section>
+$items = $deviceInfo->fetchAllDeivces($_SESSION["user_id"]);
+
+?>
+<body>
+    <section>
+        <h1>User nickname: <?php $profileInfo->fetchNickname($_SESSION["user_id"])?></h1>
+        <p>User name: <?php $profileInfo->fetchName($_SESSION["user_id"])?></p>
+        <p>User email: <?php $profileInfo->fetchEmail($_SESSION["user_id"])?></p>
+        <a href="profileSetting.php">Edit Profile</a>
+    </section>
+
+    <section>
+        <p>User device count: <?php $profileInfo->fetchDeivceCount($_SESSION["user_id"]); ?>/5</p>
+        <div class="service-boxes">
+            <div class="container">
+                <?php foreach($items as $row) { ?>
+                <div class="col-lg-4">
+                    <div class="service-box">
+                        <div class="box-inner">
+                            <img src="https://source.unsplash.com/random/500x500?sig=1" alt="">
+                            <div class="box-content">
+                                <h3 class="title"><?php echo $row["crypto_device_name"]; ?></h3>
+                                <h3 class="title"><?php echo $row["crypto_device_id"]; ?></h3>
+                                <a href="deviceEdit.php?deviceId=<?php echo $row["crypto_device_id"]; ?>" class="link-device-edit" >Edit Device</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+        <a href="deviceAdd.php">Add Device</a>
+    </section>
+</body>
 
 </body>
 </html>
