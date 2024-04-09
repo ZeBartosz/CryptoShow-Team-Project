eb56920651bbdc535abed4da528f9a40bc64a86f
OmarAldaboubi <163063942+OmarAldaboubi@users.noreply.github.com>
2024-04-09 17:50:45 +0100

    Added footer php

---
+++ phpappfolder/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p>&copy; 2024 CryptoShow. All rights reserved.</p>
+        <a href="#">About Us |</a>
+        <a href="contactus.php">Contact Us |</a>
+        <a href="#">Learn More</a>
+    </footer>
