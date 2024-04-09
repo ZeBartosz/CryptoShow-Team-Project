1dab43b0a053f250beaaca46a69ec0e6f3da8ebe
OmarAldaboubi <163063942+OmarAldaboubi@users.noreply.github.com>
2024-04-09 17:49:12 +0100

    Updated Login

--- login.php
+++ login.php
@@ -1,17 +1,23 @@
 <?php
 $title = "Login page";
-$css_file = "./css-files/dashboardStyle.css";
+$css_file = "./css-files/loginStyle.css";
+$css_filee ="./css-files/header.css";
 include_once "header.php";
 ?>
-    <div class="body">
+    <div class="content">
+        <h1>Sign in</h1>
+        <p>Sign into your CryptoShow account</p>
         <form action="./php-files/loginProcess.php" method="post">
             <label for="username">Username or Email:</label><br>
-            <input type="text" id="username" name="username"><br>
+            <input type="text" id="username" name="username"placeholder="Username"><br>
             <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password"><br><br>
-            <input type="submit" name="submit">
+            <input type="password" id="password" name="password"placeholder="Password"><br><br>
+            <button type="submit" name="submit">Login</button>
         </form>
-        <a href="./register.php">Register here</a>
+        <p>Don't have an account? <a href="register.php">Register here</a></p>
     </div>
+    <?php
+    include_once "footer.php";
+    ?>
 </body>
 </html>
