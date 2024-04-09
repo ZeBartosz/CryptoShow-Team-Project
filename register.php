eabab8bcb92800e647415b5c4ee587f6fe148327
OmarAldaboubi <163063942+OmarAldaboubi@users.noreply.github.com>
2024-04-09 17:49:21 +0100

    Updated Register

--- register.php
+++ register.php
@@ -1,27 +1,32 @@
 <?php
 $title = "Register here!";
-$css_file = "./css-files/dashboardStyle.css";
+$css_file = "./css-files/registerStyle.css";
+$css_filee = "./css-files/header.css";
 include_once "header.php";
 ?>
 <div class="body">
+<h1>Create an account</h1>
+        <p>Create a CryptoShow Account</p>
     <form action="php-files/registerProcess.php" method="post">
-        <label for="username">Username or Email:</label><br>
-        <input type="text" id="username" name="username"><br>
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name">
-            <label for="lastname">Lastname:</label>
-            <input type="text" id="lastname" name="lastname">
-        </div>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-        <label for="rptPassword">Repeat Password:</label><br>
-        <input type="password" id="rptPassword" name="rptPassword"><br><br>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email"><br><br>
-        <input type="submit" name="submit">
+        <label for="username">Username or Email:</label>
+        <input type="text" id="username" name="username"
+        placeholder="Username">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" placeholder="Firstname">
+        <label for="lastname">Lastname:</label>
+        <input type="text" id="lastname" name="lastname"placeholder="Lastname">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password"placeholder="Password">
+        <label for="rptPassword">Repeat Password:</label>
+        <input type="password" id="rptPassword" name="rptPassword"placeholder="Repeat password">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email"placeholder="Email"><br>
+        <button type="submit" name="submit">Register</button>
     </form>
-    <a href="./login.php">Log in here if you have an account</a>
+    <p>Already have an account? <a href="login.php">Log in instead</a></p>
 </div>
+<?php
+    include_once "footer.php";
+    ?>
 </body>
 </html>
