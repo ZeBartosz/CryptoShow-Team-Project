b0d82720460c4bd520dd790430ec4ac8c9be5f79
youcefabd <130198696+youcefabd@users.noreply.github.com>
2024-04-09 17:13:44 +0100

    Update popup.php

--- popup.php
+++ popup.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $_SESSION["event_date"] = $_POST["event_date"];
     $_SESSION["event_name"] = $_POST["event_name"];
     $_SESSION["event_venue"] = $_POST["event_venue"];
-
+    $_SESSION["event_description"] = $_POST["event_description"];
     // Redirect or perform other actions as needed
 }
 ?>
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </section>
         <section class="event-description">
             <h2>Description of the Event</h2>
-            <p>As the sun dipped below the horizon, the sky transformed into a breathtaking canvas of orange, pink, and purple hues. The gentle breeze carried the sweet scent of jasmine, adding to the serene ambiance. In the distance, the faint sound of a guitar strumming a melodious tune could be heard, blending harmoniously with the surroundings.</p>
+            <p><?php echo $_SESSION["event_description"]?></p>
         </section>
         <section class="event-devices">
             <h2>Devices which will be in this event:</h2>
@@ -46,3 +46,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </footer>
 </body>
 </html>
+
