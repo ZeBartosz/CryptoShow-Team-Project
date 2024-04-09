0f495823adf42c15272871a7455a790c3e195c2f
OmarAldaboubi <163063942+OmarAldaboubi@users.noreply.github.com>
2024-04-09 17:42:37 +0100

    Updated eventedit

--- eventEdit.php
+++ eventEdit.php
@@ -1,6 +1,7 @@
 <?php
 $title = "Edit Event";
-$css_file = "./css-files/dashboardStyle.css";
+$css_file = "./css-files/header.css";
+$css_filee ="./css-files/adminStyle.css";
 include_once "header.php";
 require_once "validateAdmin.php";
 include "db_connect.php";
@@ -21,21 +22,25 @@ if(isset($_GET["eventid"])) {
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 }
 ?>
-<form action="./php-files/eventEditProcess.php" method="post">
+<form class="edit-form" action="./php-files/eventEditProcess.php" method="post">
     <input type="hidden" value="<?= $result["event_id"] ?>" name="id">
     <div>
-        <label>Event name</label>
+        <label>Event Name:</label>
         <input type="text" value="<?= $result["event_name"] ?>" name="name">
     </div>
     <div>
-        <label>Event Date</label>
+        <label>Event Date:</label>
         <input type="date" value="<?= $result["event_date"] ?>" name="date">
     </div>
     <div>
-        <label>Event Venue</label>
+        <label>Event Venue:</label>
         <input type="text" value="<?= $result["event_venue"] ?>" name="venue">
     </div>
-    <button type="submit" name="submit">Update User</button>
+    <div>
+        <label class="desc">Event Description:</label>
+        <textarea type="text" value="<?= $result["event_description"] ?>" name="description"></textarea>
+    </div>
+    <button type="submit" name="submit">Update Event</button>
 </form>
 
 <footer>
