c17820e49c4553859746f3e23d5a58edfd5491cf
OmarAldaboubi <163063942+OmarAldaboubi@users.noreply.github.com>
2024-04-09 17:50:55 +0100

    updated header.php

--- phpappfolder/includes/header.php
+++ phpappfolder/includes/header.php
@@ -19,29 +19,33 @@ if(isset($_GET['logout'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title;?></title>
     <link rel="stylesheet" href="<?php echo $css_file ?>">
-    <link rel="stylesheet" href="<?php echo $css_file2 ?>">
+    <link rel="stylesheet" href="<?php echo $css_filee ?>">
+    
     <script src="./Js-Files/carousel.js" defer></script>
     <script src="./Js-Files/adminTabs.js" defer></script>
+    
     </head>
 <body>
 <header>
     <a class="logo" href="index.php"><h1>CryptoShow</h1></a>
-    <ul>
-        <?php
-        if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']) {
-            echo  "<li><a class='headerBtn' href='./admin.php'>Admin</a></li>";
-        }
-        ?>
-        <?php
-        if (isset($_SESSION['user_id'] )) {
-            echo  "<li><a class='headerBtn' href='./profile.php'>Profile</a></li>";
-            echo "<li><a class='headerBtn' href='index.php?logout=true'>Logout</a></button></li>";
-        } else {
-          echo  "<li><a class='headerBtn' href='register.php'>Register</a></li>";
-          echo  "<li><a class='headerBtn' href='login.php'>Login</a></li>";
-        }
-        ?>
+    <nav class ="navigate">
+        <ul class = "header-menu">
+            <?php
+            if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']) {
+                echo  "<li><a class='headerBtn' href='./admin.php'>Admin</a></li>";
+            }
+            ?>
+            <?php
+            if (isset($_SESSION['user_id'] )) {
+                echo  "<li><a class='headerBtn' href='./profile.php'>Profile</a></li>";
+                echo "<li><a class='headerBtn' href='index.php?logout=true'>Logout</a></button></li>";
+            } else {
+              echo  "<li><a class='headerBtn' href='register.php'>Register</a></li>";
+              echo  "<li><a class='headerBtn' href='login.php'>Login</a></li>";
+            }
+            ?>
 
     </ul>
+    </nav>
 
 </header>
