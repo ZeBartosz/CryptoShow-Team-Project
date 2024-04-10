6eb8710dd9403f29040f4f6ef0cad23e5a712d64
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-10 20:54:39 +0100

    Made significant changes after changing code to MVC

--- admin.php
+++ admin.php
@@ -1,11 +1,51 @@
 <?php
 $title = "Admin Page";
-$css_file = "./css-files/adminStyle.css";
-$css_filee = "./css-files/header.css";
+$css_file = "./css-files/dashboardStyle.css";
 include_once "header.php";
 require_once "validateAdmin.php";
 include "db_connect.php";
+include_once "userView.php";
+include_once "userModel.php";
+include_once "eventView.php";
+include_once "eventController.php";
+include_once "deviceController.php";
+include_once "deviceView.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST["delete_user"])) {
+        $user_id = $_POST["delete_user"];
+        $model = new UserModel();
+        $model->deleteUserInfo($user_id);
+        header("Location: {$_SERVER['PHP_SELF']}");
+        exit();
+    }
+    if(isset($_POST["delete_event"])) {
+        $event_id = $_POST["delete_event"];
+        $model = new EventModel();
+        $model->deleteEvent($event_id);
+        header("Location: {$_SERVER['PHP_SELF']}");
+        exit();
+    }
+    if(isset($_POST["publish"])) {
+        $event_id = $_POST["publish"];
+        $model = new EventModel();
+        $model->publishEvent($event_id);
+        header("Location: {$_SERVER['PHP_SELF']}");
+        exit();
+    }
+    if(isset($_POST["delete_device"])) {
+        $device_id = $_POST["delete_device"];
+        $model = new DeviceModel();
+        $model->deleteDeviceInfo($device_id);
+        header("Location: {$_SERVER['PHP_SELF']}");
+        exit();
+    }
+
+}
+
+
 ?>
+<link rel="stylesheet" href="./css-files/adminStyle.css">
 <?php if(isset($_SESSION["message"])) { ?>
     <h5><?= $_SESSION['message'] ?></h5> <?php
     unset($_SESSION["message"]);
@@ -30,53 +70,28 @@ include "db_connect.php";
                     <th>Email</th>
                     <th>Device count</th>
                     <th>Registered</th>
-                    <th colspan="2">Edit</th>
+                    <th>Edit</th>
                 </tr>
                 </thead>
                 <tbody>
                 <?php
-                $query = "SELECT * FROM registered_user";
-                $stmt = $pdo->prepare($query);
-                $stmt->execute();
-
-
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                if($result) {
-                    foreach($result as $row) {
-                        ?>
-                        <tr>
-                            <td><?php if($row['is_admin']){echo "Yes";}else{echo "No";}?></td>
-                            <td><?= $row['user_id']?></td>
-                            <td><?= $row['user_nickname']?></td>
-                            <td><?= $row['user_name']?></td>
-                            <td><?= $row['user_email']?></td>
-                            <td><?= $row['user_device_count']?></td>
-                            <td><?= $row['user_registered_timestamp']?></td>
-                            <td><a href="userEdit.php?id=<?= $row['user_id']; ?>"><button type= "submit">Edit</button></a></td>
-                            <td>
-                            <form action="./php-files/userEditProcess.php" method="post">
-                            <button type="delete" name="delete" value="<?=$row['user_id']?>" onclick="return confirm('Are you sure?')">Delete</button>
-                            </form>
-                            
-                        </tr>
-                        <?php
-                    }
-                } else {
-                    ?>
-                    <tr>
-                        <td colspan="6">No Record</td>
-                    </tr>
-                    <?php
+                    $userController = new UserController();
+                    $userView = new UserView($userController);
+                    $userDisplayed = $userView->displayAllUserInfo();
+                if(!$userDisplayed === true) {
+                    echo "<tr>";
+                    echo "<td colspan='7'>No Record</td>";
+                    echo "</tr>";
                 }
                 ?>
+
                 </tbody>
             </table>
         </div>
         <div class="content">
             <h2>Events</h2>
             <div>
-                <a href="./addEvent.php"><button class= "addEventBtn">Add Event</button></a>
+                <a href="./addEvent.php"><button>Add Event</button></a>
             </div>
             <table>
                 <thead>
@@ -87,48 +102,18 @@ include "db_connect.php";
                     <th>Event Date</th>
                     <th>Event Venue</th>
                     <th>Published</th>
-                    <th colspan="3">Edit</th>
+                    <th>Edit</th>
                 </tr>
                 </thead>
                 <tbody>
                 <?php
-                $query = "SELECT * FROM event";
-                $stmt = $pdo->prepare($query);
-                $stmt->execute();
-
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                if($result) {
-                    foreach($result as $row) {
-                        ?>
-                        <tr>
-                            <td><?= $row['event_id']?></td>
-                            <td><?= $row['event_name']?></td>
-                            <td><?= $row['event_description']?></td>
-                            <td><?= $row['event_date']?></td>
-                            <td><?= $row['event_venue']?></td>
-                            <td><?php if($row['is_published']){echo "Yes";}else{echo "No";}?></td>
-                            <td><a href="eventEdit.php?eventid=<?= $row['event_id']; ?>"><button type="submit">Edit</button></a></td>
-                            <td>
-                                <form action="./php-files/eventEditProcess.php" method="post">
-                                    <button type="delete" name="delete" value="<?=$row['event_id']?>" onclick="return confirm('Are you sure?')">Delete</button>
-                                </form>
-                            </td>
-                            <td>
-                                <form action="./php-files/eventEditProcess.php" method="post">
-                                    <button type="submit" name="publish" value="<?=$row['event_id']?>" onclick="return confirm('Are you sure?')">Publish</button>
-                                </form>
-                            </td>
-
-                        </tr>
-                        <?php
-                    }
-                } else {
-                    ?>
-                    <tr>
-                        <td colspan="4">No Record</td>
-                    </tr>
-                    <?php
+                $eventController = new EventController();
+                $userView = new EventView($eventController);
+                $eventDisplayed = $userView->displayAllEvents();
+                if(!$eventDisplayed === true) {
+                    echo "<tr>";
+                    echo "<td colspan='6'>No Record</td>";
+                    echo "</tr>";
                 }
                 ?>
                 </tbody>
@@ -139,41 +124,22 @@ include "db_connect.php";
             <table>
                 <thead>
                 <tr>
+                    <th>Device Visible</th>
+                    <th>Device ID</th>
+                    <th>Device Name</th>
                     <th>User ID</th>
-                    <th>Nickname</th>
-                    <th>Fullname</th>
-                    <th>Email</th>
-                    <th>Device count</th>
-                    <th>Registered</th>
+                    <th>Device Registered</th>
                 </tr>
                 </thead>
                 <tbody>
                 <?php
-                $query = "SELECT * FROM registered_user";
-                $stmt = $pdo->prepare($query);
-                $stmt->execute();
-
-                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                if($result) {
-                    foreach($result as $row) {
-                        ?>
-                        <tr>
-                            <td><?= $row['user_id']?></td>
-                            <td><?= $row['user_nickname']?></td>
-                            <td><?= $row['user_name']?></td>
-                            <td><?= $row['user_email']?></td>
-                            <td><?= $row['user_device_count']?></td>
-                            <td><?= $row['user_registered_timestamp']?></td>
-                        </tr>
-                        <?php
-                    }
-                } else {
-                    ?>
-                    <tr>
-                        <td colspan="6">No Record</td>
-                    </tr>
-                    <?php
+                $deviceController = new DeviceController();
+                $deviceView = new DeviceView($deviceController);
+                $deviceDisplayed = $deviceView->displayAllDeviceInfo();
+                if(!$eventDisplayed === true) {
+                    echo "<tr>";
+                    echo "<td colspan='6'>No Record</td>";
+                    echo "</tr>";
                 }
                 ?>
                 </tbody>
@@ -188,9 +154,9 @@ include "db_connect.php";
 </div>
 
 
-<?php
-    include_once "footer.php";
-    ?>
+<footer>
+    <p>&copy; 2024 CryptoShow. All rights reserved.</p>
+</footer>
 
 </body>
 
