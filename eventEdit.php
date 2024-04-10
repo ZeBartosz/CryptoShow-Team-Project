0fcaa637b96be2802ae7be912c3f2f6a818b425d
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-10 21:08:17 +0100

    Made significant change after changing code to MVC

--- eventEdit.php
+++ eventEdit.php
@@ -1,51 +1,72 @@
 <?php
-$title = "Edit Event";
-$css_file = "./css-files/header.css";
-$css_filee ="./css-files/adminStyle.css";
+$css_file = "./css-files/dashboardStyle.css";
 include_once "header.php";
 require_once "validateAdmin.php";
 include "db_connect.php";
+include "eventController.php";
 ?>
 
 <?php
-if(isset($_GET["eventid"])) {
-    $event_id = $_GET["eventid"];
+$controller = new EventController();
+if(isset($_GET["eventId"])) {
+    $event_id = $_GET["eventId"];
 
-    $query = "SELECT * FROM event WHERE event_id=:event";
-
-    $stmt = $pdo->prepare($query);
-    $data = [
-      ":event" => $event_id
-    ];
-    $stmt->execute($data);
-
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $event_info = $controller->getEvent($event_id);
+    if(!$event_info) {
+        $_SESSION["message"] = "Invalid event ID";
+        header("location: ./admin.php");
+        exit();
+    }
 }
-?>
-<form class="edit-form" action="./php-files/eventEditProcess.php" method="post">
-    <input type="hidden" value="<?= $result["event_id"] ?>" name="id">
-    <div>
-        <label>Event Name:</label>
-        <input type="text" value="<?= $result["event_name"] ?>" name="name">
-    </div>
-    <div>
-        <label>Event Date:</label>
-        <input type="date" value="<?= $result["event_date"] ?>" name="date">
-    </div>
-    <div>
-        <label>Event Venue:</label>
-        <input type="text" value="<?= $result["event_venue"] ?>" name="venue">
-    </div>
-    <div>
-        <label class="desc">Event Description:</label>
-        <textarea type="text" value="<?= $result["event_description"] ?>" name="description"></textarea>
-    </div>
-    <button type="submit" name="submit">Update Event</button>
-</form>
 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $event_id = $_POST["id"];
+    $event_name = $_POST["name"];
+    $event_date = $_POST["date"];
+    $event_venue = $_POST["venue"];
+    $event_description = htmlspecialchars($_POST["description"], ENT_QUOTES, "UTF-8");
+    $controller = new EventController();
+    $result = $controller->setEventInfo($event_id, $event_date, $event_name, $event_description, $event_date, $event_venue);
+
+    if($result === true) {
+        $_SESSION["message"] = "Successfully edited user information";
+        header("location: ./admin.php");
+        exit();
+    } else {
+        header("location: ./admin.php");
+        $_SESSION["message"] = "Error updating user information";
+    }
+}
+
+?>
+<?php if(isset($_SESSION["message"])) { ?>
+    <h5><?= $_SESSION["message"] ?></h5> <?php
+    unset($_SESSION["message"]);
+} ?>
+<form method="post">
+    <input type="hidden" value="<?= $event_info["event_id"] ?>" name="id">
+    <div>
+        <label>Event name</label>
+        <input type="text" value="<?= $event_info["event_name"] ?>" name="name">
+    </div>
+    <div>
+        <label>Event Description</label>
+        <textarea name="description"><?= $event_info["event_description"] ?></textarea>
+    </div>
+    <div>
+        <label>Event Date</label>
+        <input type="date" value="<?= $event_info["event_date"] ?>" name="date">
+    </div>
+    <div>
+        <label>Event Venue</label>
+        <input type="text" value="<?= $event_info["event_venue"] ?>" name="venue">
+    </div>
+    <button type="submit" name="submit">Update User</button>
+</form>
+<a href="./admin.php"><button>Cancel</button></a>
 <?php
-    include_once "footer.php";
-    ?>
+include_once "footer.php";
+?>
 </body>
 
 </html>
