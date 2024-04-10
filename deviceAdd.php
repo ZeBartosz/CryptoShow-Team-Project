8b9d8f36f5b46c05d6143269ab7751a9559df113
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-10 20:55:43 +0100

    Made changes to include image uploading and fixed some typos

--- deviceAdd.php
+++ deviceAdd.php
@@ -2,63 +2,27 @@
     $title = "Add Device";
     $css_file = "./css-files/dashboardStyle.css";
     include_once "header.php";
-    include "deviceProcess.php";
+    include "deviceModel.php";
     include "deviceController.php";
     include "deviceView.php";
-    $fileDestination = '';
 
-
-    if(isset($_POST["submit"])) {
-
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
         $id = $_SESSION["user_id"];
-
         $name = htmlspecialchars($_POST["name"], ENT_QUOTES, "UTF-8");
-        $isVisible = isset($_POST["visible"])? 1 : 0;
+        $is_visible = isset($_POST["visible"])? 1 : 0;
 
-        $id = $_SESSION["user_id"];
-        $file = $_FILES['file'];
-
-        $fileName = $file['name'];
-        $fileTmpName = $file['tmp_name'];
-        $fileSize = $file['size'];
-        $fileError = $file['error'];
-        $fileType = $file['type'];
-
-        $fileExt = explode('.', $fileName);
-        $fileActualExt = strtolower(end($fileExt));
-        $allowed = array('jpg', 'jpeg'. 'png');
-        $fileDestination = '';
-
-        if(in_array($fileActualExt, $allowed)){
-            if ($fileError === 0){
-                if ($fileSize < 3000000){
-                    $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-                    $randomString = substr(str_shuffle($str_result), 0, 5);
-                    $fileNameNew = "profile".$id.$randomString.".".$fileActualExt;
-                    $fileDestination = 'device-images/'.$fileNameNew;
-                    move_uploaded_file($fileTmpName, $fileDestination);
-                } else {
-                    header("Location: profile.php?error=imageTooBig");
-                    exit();
-                }
-
-            } else {
-                header("Location: profile.php?error=ErrorImage");
-                exit();
-            }
-        } else {
-            header("Location: profile.php?error=wrong");
-            exit();
-        }
+        $target_dir = "upload/";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
 
 
-        $deviceInfo = new DeviceController($id);
+        $deviceInfo = new DeviceController();
 
-        $deviceInfo->insertDevice($name, $fileDestination, $isVisible);
+        $deviceInfo->setForeignId($id);
+        $deviceInfo->insertDevice($name, $target_file, $is_visible);
 
 
     }
-
 ?>
 
 
@@ -67,11 +31,11 @@
             <div class="wrapper">
                 <div class="profile-settings">
                     <h3>ADD DEVICE</h3>
-                    <form method="post">
-                        <P>Device name!</P>
+                    <form method="post" enctype="multipart/form-data">
+                        <P>Change your device name!</P>
                         <input type="text" name="name" placeholder="device name...">
-                        <p>Device image!</p>
-                        <input type="file" name="file" value="" >
+                        <p>Change your device image!</p>
+                        <input type="file" name="image" placeholder="device image..." >
                         <p>Change your device shown!</p>
                         <input type="checkbox" name="visible" value="checked" >
                         <button type="submit" name="submit">ADD</button>
