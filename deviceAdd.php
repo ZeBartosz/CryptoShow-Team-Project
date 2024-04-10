8d13e754c041f24f3153460787a440e029ee8231
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2024-04-10 19:48:08 +0100

    Update deviceAdd.php

--- deviceAdd.php
+++ deviceAdd.php
@@ -5,20 +5,58 @@
     include "deviceProcess.php";
     include "deviceController.php";
     include "deviceView.php";
+    $fileDestination = '';
+
 
     if(isset($_POST["submit"])) {
 
         $id = $_SESSION["user_id"];
 
         $name = htmlspecialchars($_POST["name"], ENT_QUOTES, "UTF-8");
-        $image = htmlspecialchars($_POST["image"], ENT_QUOTES, "UTF-8");
-        $visible = htmlspecialchars($_POST["visible"], ENT_QUOTES, "UTF-8" );
+        $isVisible = isset($_POST["visible"])? 1 : 0;
+
+        $id = $_SESSION["user_id"];
+        $file = $_FILES['file'];
+
+        $fileName = $file['name'];
+        $fileTmpName = $file['tmp_name'];
+        $fileSize = $file['size'];
+        $fileError = $file['error'];
+        $fileType = $file['type'];
+
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+        $allowed = array('jpg', 'jpeg'. 'png');
+        $fileDestination = '';
+
+        if(in_array($fileActualExt, $allowed)){
+            if ($fileError === 0){
+                if ($fileSize < 3000000){
+                    $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+                    $randomString = substr(str_shuffle($str_result), 0, 5);
+                    $fileNameNew = "profile".$id.$randomString.".".$fileActualExt;
+                    $fileDestination = 'device-images/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                } else {
+                    header("Location: profile.php?error=imageTooBig");
+                    exit();
+                }
+
+            } else {
+                header("Location: profile.php?error=ErrorImage");
+                exit();
+            }
+        } else {
+            header("Location: profile.php?error=wrong");
+            exit();
+        }
+
 
         $deviceInfo = new DeviceController($id);
 
-        $deviceInfo->insertDevice($name, $image, $visible);
+        $deviceInfo->insertDevice($name, $fileDestination, $isVisible);
 
-        header("location: profile.php?error=none");
+
     }
 
 ?>
@@ -29,13 +67,13 @@
             <div class="wrapper">
                 <div class="profile-settings">
                     <h3>ADD DEVICE</h3>
-                    <form action="<?php $_SERVER["PHP_SELF"]; ?>" method="post">
-                        <P>Change your device name!</P>
+                    <form method="post">
+                        <P>Device name!</P>
                         <input type="text" name="name" placeholder="device name...">
-                        <p>Change your device image!</p>
-                        <input type="text" name="image" placeholder="device image..." >
+                        <p>Device image!</p>
+                        <input type="file" name="file" value="" >
                         <p>Change your device shown!</p>
-                        <input type="text" name="visible" placeholder="device visible...TRUE/FALSE" >
+                        <input type="checkbox" name="visible" value="checked" >
                         <button type="submit" name="submit">ADD</button>
                     </form>
                 </div>
