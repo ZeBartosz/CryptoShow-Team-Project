1813dce224e2be8eacb65cd476fe549a116bc8fd
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-13 23:53:11 +0100

    Update popup.php

--- popup.php
+++ popup.php
@@ -2,8 +2,9 @@
 $title = " event details";
 $css_file = "./css-files/dashboardStyle.css";
 $css_filee = "./css-files/popup.css";
-include_once "./includes/header.php";
-
+include_once "header.php";
+include_once "userView.php";
+include_once "userController.php";
 
 
 
@@ -12,10 +13,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $_SESSION["event_name"] = $_POST["event_name"];
     $_SESSION["event_venue"] = $_POST["event_venue"];
     $_SESSION["event_description"] = $_POST["event_description"];
-    // Redirect or perform other actions as needed
+    $_SESSION["event_id"] = $_POST["event_id"];
 }
 ?>
-
         <div class="container">
         <h1 class="title_of_the_page"><?php echo  $_SESSION["event_name"]?></h1>
         <section class="event-info">
@@ -27,23 +27,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <p><?php echo $_SESSION["event_description"]?></p>
         </section>
         <section class="event-devices">
-            <h2>Devices which will be in this event:</h2>
-            <table>
-                <tr>
-                    <th>owner of the device</th>
-                    <th>The device he is bringing</th>
-                </tr>
-                <tr>
-                    <td>youcef ab</td>
-                    <td>router</td>
-                </tr>
-                <!-- Additional rows can be added here -->
-            </table>
+            <?php
+            $event_id = $_SESSION["event_id"];
+            $userView = new UserView(new UserController());
+            $userView->displayAttendingUsers($event_id);
+            ?>
         </section>
     </div>
-    <footer>
-        <p>&copy; 2024 CryptoShow. All rights reserved.</p>
-    </footer>
+
 </body>
 </html>
 
