f00bf7c0ae6fa05136e550bb7d50a5d5fd0864e3
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2024-04-13 20:29:40 +0100

    rename profileinfo.php to profileModel.php and add setDeviceCount function

---
+++ includes/profileModel.php
@@ -0,0 +1,68 @@
+<?php
+
+include_once "dbh.php";
+
+class ProfileModel extends Dbh
+{
+
+    protected function getProfileInfo($userId)
+    {
+
+        $sql = "SELECT * FROM registered_user WHERE user_id = ?;";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->bindParam(":userId", $userId, PDO::PARAM_STR);
+
+        if (!$stmt->execute(array($userId))) {
+            $stmt = null;
+            header("Location: profile.php?error=stmtfailed");
+            exit();
+        }
+
+        if ($stmt->rowCount() == 0) {
+            $stmt = null;
+            header("Location: ../php-files/profile.php?error=profilenotFound");
+            exit();
+        }
+
+        $profileData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $profileData;
+    }
+
+
+    protected function setNewProfileInfo($userId, $userNickname, $userName, $userEmail, $pwd)
+    {
+
+        if(empty($pwd)) {
+            $stmt = $this->connect()->prepare('UPDATE registered_user SET user_nickname = ?, user_name = ?, user_email = ? WHERE user_id = ?;');
+            if (!$stmt->execute(array($userNickname, $userName, $userEmail, $userId))) {
+                $stmt = null;
+                header("Location: ../php-files/profile.php?error=stmtfaied");
+                exit();
+            }
+        } else {
+            $hashedPwd = password_hash($pwd,PASSWORD_DEFAULT);
+            $stmt = $this->connect()->prepare('UPDATE registered_user SET user_nickname = ?, user_name = ?, user_email = ?, user_hashed_password = ? WHERE user_id = ?;');
+            if (!$stmt->execute(array($userNickname, $userName, $userEmail, $hashedPwd, $userId))) {
+                $stmt = null;
+                header("Location: ../php-files/profile.php?error=stmtfaied");
+                exit();
+            }
+        }
+
+    }
+
+    protected function setDeviceCount($userId, $deviceCount)
+    {
+
+            $stmt = $this->connect()->prepare('UPDATE registered_user SET user_device_count = ? WHERE user_id = ?;');
+            if (!$stmt->execute(array($deviceCount, $userId))) {
+                $stmt = null;
+                header("Location: ../php-files/profile.php?error=stmtfaied");
+                exit();
+            }
+        }
+
+
+
+    }
