102608a75ebea338a29f2526edcc1e71bad266f3
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-14 17:31:39 +0100

    Update validateSession.php

--- includes/validateSession.php
+++ includes/validateSession.php
@@ -1,6 +1,15 @@
 <?php
-if(isset($_SESSION["user_id"])) {
 
-} else {
-    header("location: index.php");
+function isLoggedIn() {
+    if (isset($_SESSION['user_id'])) {
+        header('Location: index.php');
+        exit();
+    }
+}
+
+function isNotLoggedIn() {
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: index.php');
+        exit();
+    }
 }
