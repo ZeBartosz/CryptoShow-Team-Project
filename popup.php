3be9bfc6f2d8f1bad75029dc7c1d91c20f459730
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-14 17:35:29 +0100

    Update popup.php

--- popup.php
+++ popup.php
@@ -9,26 +9,26 @@ include_once "userController.php";
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $_SESSION["event_date"] = $_POST["event_date"];
-    $_SESSION["event_name"] = $_POST["event_name"];
-    $_SESSION["event_venue"] = $_POST["event_venue"];
-    $_SESSION["event_description"] = $_POST["event_description"];
-    $_SESSION["event_id"] = $_POST["event_id"];
+    $event_date = $_POST["event_date"];
+    $event_name = $_POST["event_name"];
+    $event_venue = $_POST["event_venue"];
+    $event_description = $_POST["event_description"];
+    $event_id = $_POST["event_id"];
 }
 ?>
         <div class="container">
-        <h1 class="title_of_the_page"><?php echo  $_SESSION["event_name"]?></h1>
+        <h1 class="title_of_the_page"><?php echo  $event_name?></h1>
         <section class="event-info">
-            <p><strong>Location:</strong> <?php echo  $_SESSION["event_venue"] ?></p>
-            <p><strong>Date:</strong> <?php echo $_SESSION["event_date"]?></p>
+            <p><strong>Location:</strong> <?php echo $event_venue ?></p>
+            <p><strong>Date:</strong> <?php echo $event_date?></p>
+            <a href="eventList.php">Go back</a>
         </section>
         <section class="event-description">
             <h2>Description of the Event</h2>
-            <p><?php echo $_SESSION["event_description"]?></p>
+            <p><?php echo $event_description?></p>
         </section>
         <section class="event-devices">
             <?php
-            $event_id = $_SESSION["event_id"];
             $userView = new UserView(new UserController());
             $userView->displayAttendingUsers($event_id);
             ?>
