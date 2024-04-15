95b1f512fff3b583f82a449fe40c0606411a5cdc
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-15 16:29:55 +0100

    fix typos

--- includes/userView.php
+++ includes/userView.php
@@ -67,7 +67,7 @@ class UserView extends UserController {
                             <th>User who will be attending</th>
                         </tr>
                     </thead>
-                    <tbody>';
+                    </tbody>';
             $user_info = $this->controller->getForeignUserInfo($user_info);
             foreach ($user_info as $user) {
                 echo "<tr>
@@ -81,7 +81,7 @@ class UserView extends UserController {
                             <th>No attending Users</th>
                         </tr>
                     </thead>
-                    <tbody>';
+                    </tbody>';
         }
     }
 }
