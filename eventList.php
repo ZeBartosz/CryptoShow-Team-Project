6f826fcff9718e677a081b4233e2723d8a265aba
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-15 14:10:26 +0100

    Add function to check if event is published and fixed indentations

--- eventList.php
+++ eventList.php
@@ -29,51 +29,53 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 <div class="container">
-            <div class="event-list">
-    <?php foreach ($results as $row) { ?>
+    <div class="event-list">
+        <?php if(empty($results)) { ?>
+            <div class="event-location">Currently there are no events</div>
+        <?php } ?>
+        <?php foreach ($results as $row) { ?>
+            <?php if($row["is_published"]) { ?>
                 <li class="event">
-                    <div class="event-date"><?php echo htmlspecialchars($row["event_date"]); ?></div>
-                    <div class="event-title"><?php echo htmlspecialchars($row["event_name"]); ?></div>
-                    <div class="event-location"><?php echo htmlspecialchars($row["event_venue"]); ?></div>
-                    <div class="event-location"><?php if(isset($_SESSION["user_id"]) && $row["event_id"]) {
-                        $isAttending = new UserController();
-                        $isAttending = $isAttending->isAttending($_SESSION["user_id"], $row["event_id"]);
-                        if($isAttending) {
-                            echo "You are attending";
-                        } else {
-                            echo "You are Not attending";
-                        }
-                    }?></div>
-                    <?php
-                    if(isset($_SESSION["user_id"]) && $row["event_id"]){
-                        $isAttending = new UserController();
-                        $isAttending = $isAttending->isAttending($_SESSION["user_id"], $row["event_id"]);
-                        if(!$isAttending) { ?>
-                            <form method="post">
-                                <input name="event_id" type="hidden" value='<?= $row["event_id"] ?>'>
-                                <button name="book" type="book" class="book-button">Book Now</button>
-                            </form>
-                        <?php } else {
-                        }
-                        ?>
-                    <?php } ?>
-            <br>
-            <form action="popup.php" method="post">
-                <input type="hidden" name="event_date" value="<?php echo htmlspecialchars($row["event_date"]); ?>">
-                <input type="hidden" name="event_name" value="<?php echo htmlspecialchars($row["event_name"]); ?>">
-                <input type="hidden" name="event_venue" value="<?php echo htmlspecialchars($row["event_venue"]); ?>">
-                <input type="hidden" name="event_description" value="<?php echo htmlspecialchars($row["event_description"]); ?>">
-                <input type="hidden" name="event_id" value="<?php echo htmlspecialchars($row["event_id"]); ?>">
-                <input type="submit" name="submit_button" class="info-button" value="For more information click here">
-
-            </form>
-        </li>
-
-            
-    <?php } ?>
+                <div class="event-date"><?php echo htmlspecialchars($row["event_date"]); ?></div>
+                <div class="event-title"><?php echo htmlspecialchars($row["event_name"]); ?></div>
+                <div class="event-location"><?php echo htmlspecialchars($row["event_venue"]); ?></div>
+                <div class="event-location"><?php if(isset($_SESSION["user_id"]) && $row["event_id"]) {
+                    $isAttending = new UserController();
+                    $isAttending = $isAttending->isAttending($_SESSION["user_id"], $row["event_id"]);
+                    if($isAttending) {
+                        echo "You are attending";
+                    } else {
+                        echo "You are Not attending";
+                    }
+                }?>
+                </div>
+                <?php
+                if(isset($_SESSION["user_id"]) && $row["event_id"]){
+                    $isAttending = new UserController();
+                    $isAttending = $isAttending->isAttending($_SESSION["user_id"], $row["event_id"]);
+                    if(!$isAttending) { ?>
+                        <form method="post">
+                            <input name="event_id" type="hidden" value='<?= $row["event_id"] ?>'>
+                            <button name="book" type="book" class="book-button">Book Now</button>
+                        </form>
+                    <?php } else {
+                    } ?>
+                <?php } ?>
+            <?php } ?>
+            <?php if($row["is_published"]) { ?>
+                <form action="popup.php" method="post">
+                    <input type="hidden" name="event_date" value="<?php echo htmlspecialchars($row["event_date"]); ?>">
+                    <input type="hidden" name="event_name" value="<?php echo htmlspecialchars($row["event_name"]); ?>">
+                    <input type="hidden" name="event_venue" value="<?php echo htmlspecialchars($row["event_venue"]); ?>">
+                    <input type="hidden" name="event_description" value="<?php echo htmlspecialchars($row["event_description"]); ?>">
+                    <input type="hidden" name="event_id" value="<?php echo htmlspecialchars($row["event_id"]); ?>">
+                    <input type="submit" name="submit_button" class="info-button" value="For more information click here"></form>
+            <?php } ?>
+            </li>
+        <?php } ?>
     </div>
-</ul>
+    </ul>
 </div>
-    </body>
+</body>
 </html>
 
