d2aa2ad458b838667a85ad81ecb9c8ec7fd72b10
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-16 12:18:46 +0100

    Update validateSession.php

--- includes/validateSession.php
+++ includes/validateSession.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Redirects the user to the home page if they are already logged in.
+ * 
+ * This function checks if the session contains a 'user_id' key, indicating that
+ * the user is currently logged in. If they are logged in, it redirects them to
+ * the home page and prevents further script execution.
+ */
 function isLoggedIn() {
     if (isset($_SESSION['user_id'])) {
         header('Location: index.php');
@@ -7,6 +14,13 @@ function isLoggedIn() {
     }
 }
 
+/**
+ * Redirects the user to the home page if they are not logged in.
+ * 
+ * This function checks if the session does not contain a 'user_id' key, indicating that
+ * the user is not logged in. If they are not logged in, it redirects them to the
+ * home page and prevents further script execution.
+ */
 function isNotLoggedIn() {
     if (!isset($_SESSION['user_id'])) {
         header('Location: index.php');
