b5b047ee5ff4c03c7fd2d8c417377be077eac90f
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-16 12:00:22 +0100

    Update eventModel.php

--- includes/eventModel.php
+++ includes/eventModel.php
@@ -1,8 +1,21 @@
 <?php
 
 include_once "dbh.php";
+
+/**
+ * EventModel handles all database interactions related to events within the application.
+ * 
+ * This class extends from Dbh (Database Handler), which manages database connections. It
+ * provides methods for CRUD operations related to event management such as adding, updating,
+ * deleting, publishing, and retrieving event data.
+ */
 class EventModel extends Dbh {
 
+    /**
+     * Deletes an event based on its ID.
+     *
+     * @param int $event_id The ID of the event to be deleted.
+     */
     protected function deleteEvent($event_id){
         try {
             $query = "DELETE FROM event WHERE event_id = :eventid";
@@ -18,6 +31,11 @@ class EventModel extends Dbh {
         }
     }
 
+    /**
+     * Publishes an event by setting its 'is_published' status to true.
+     *
+     * @param int $event_id The ID of the event to be published.
+     */
     protected function setPublishEvent($event_id) {
         try {
             $query = "UPDATE event SET is_published = 1 WHERE event_id = :eventid";
@@ -32,6 +50,12 @@ class EventModel extends Dbh {
         }
     }
 
+    /**
+     * Books an event for a user by inserting a record into the user_event table.
+     *
+     * @param int $user_id The user's ID.
+     * @param int $event_id The event's ID.
+     */
     protected function setbookEvent($user_id, $event_id) {
         try {
             $query = "INSERT INTO user_event (fk_user_id, fk_event_id) VALUES (?, ?);";
@@ -44,6 +68,11 @@ class EventModel extends Dbh {
         }
     }
 
+    /**
+     * Retrieves all event information from the database.
+     *
+     * @return array|void An array of all event data or void if an error occurs.
+     */
     protected function getAllEventInfo() {
         try {
             $query = "SELECT * FROM event";
@@ -57,6 +86,12 @@ class EventModel extends Dbh {
         }
     }
 
+    /**
+     * Retrieves a single event by its ID.
+     *
+     * @param int $event_id The ID of the event to fetch.
+     * @return array|null Event data or null if an error occurs.
+     */
     protected function getEvent($event_id) {
         try {
             $query = "SELECT * FROM event WHERE event_id = :event_id";
@@ -72,6 +107,16 @@ class EventModel extends Dbh {
         }
     }
 
+    /**
+     * Updates an event's details in the database.
+     *
+     * @param int $event_id The ID of the event.
+     * @param string $event_name The name of the event.
+     * @param string $event_description The description of the event.
+     * @param string $event_date The date of the event.
+     * @param string $event_venue The venue of the event.
+     * @return bool True on successful update.
+     */
     protected function setEventInfo($event_id, $event_name, $event_description, $event_date, $event_venue) {
         try {
             $query = "UPDATE event SET event_name = :event_name, event_description = :event_description, event_date = :event_date, event_venue = :event_venue WHERE event_id = :event_id";
@@ -93,6 +138,16 @@ class EventModel extends Dbh {
         }
     }
 
+    /**
+     * Adds a new event to the database.
+     *
+     * @param string $event_name The name of the event.
+     * @param string $event_description The description of the event.
+     * @param string $event_date The date of the event.
+     * @param string $event_venue The venue of the event.
+     * @param bool $is_published Whether the event is published.
+     * @return bool True on successful addition.
+     */
     protected function addEvent($event_name, $event_description, $event_date, $event_venue, $is_published) {
         try {
             $query = "INSERT INTO event (event_name, event_date, event_description, event_venue, is_published) VALUES (:event_name, :event_date, :event_description, :event_venue, :is_published) ";
@@ -113,6 +168,12 @@ class EventModel extends Dbh {
         }
     }
 
+    /**
+     * Searches for events in the database by a keyword that matches multiple fields.
+     *
+     * @param string $search_keyword The keyword to search for.
+     * @return array|void An array of events that match the keyword, or void if an error occurs.
+     */
     protected function searchEventByKeyword($search_keyword) {
         try {
             $query = "SELECT * FROM event WHERE event_name LIKE :search_keyword OR event_description LIKE :search_keyword OR event_venue LIKE :search_keyword OR event_id LIKE :search_keyword";
