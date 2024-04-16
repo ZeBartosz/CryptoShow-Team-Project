b570db9f027f63d1057bed30cbab202e7945b3f9
OmarAldaboubi <163063942+OmarAldaboubi@users.noreply.github.com>
2024-04-16 14:03:38 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer>
         <p>&copy; 2024 CryptoShow. All rights reserved.</p>
-        <a href="#">About Us |</a>
+        <a href="./aboutus.php">About Us |</a>
         <a href="contactus.php">Contact Us |</a>
-        <a href="#">Learn More</a>
+        <a href="./learnMore.php">Learn More</a>
     </footer>
