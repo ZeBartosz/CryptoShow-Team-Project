65906808719046e6e9f29b1895c75b2b492063a4
Krzysztof Romanowski <161979368+Tenarien@users.noreply.github.com>
2024-04-16 12:22:22 +0100

    Update dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,7 +1,21 @@
 <?php
 
+/**
+ * Dbh (Database Handler) class responsible for creating and managing database connections.
+ */
 class Dbh {
 
+    /**
+     * Establishes a PDO connection to a MySQL database.
+     * 
+     * This method attempts to create a new PDO instance using specified database credentials.
+     * It constructs the Data Source Name (DSN) with parameters for the host and database name,
+     * then tries to connect using the username and password. If the connection is successful,
+     * it returns the PDO object. If the connection fails, it terminates the script and outputs
+     * an error message.
+     *
+     * @return PDO A PDO object on success, or terminates the script on failure.
+     */
     protected function connect(){
         try {
             $host = "localhost";
